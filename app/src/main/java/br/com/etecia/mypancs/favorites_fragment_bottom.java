ddb866329fc43bef81e957905c13c957ca2e484b
Leticiavvs13 <leticiavsvieira@gmail.com>
2022-09-06 20:23:13 -0300

    first commit

---
+++ app/src/main/java/br/com/etecia/mypancs/favorites_fragment_bottom.java
@@ -0,0 +1,20 @@
+package br.com.etecia.mypancs;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class favorites_fragment_bottom extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_favorites_bottom, container, false);
+        return v;
+    }
+}
---
+++ app/src/main/java/br/com/etecia/mypancs/sobre_fragment_tab.java
@@ -0,0 +1,20 @@
+package br.com.etecia.mypancs;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class sobre_fragment_tab extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.sobre_fragment_tab, container, false);
+        return v;
+    }
+}
